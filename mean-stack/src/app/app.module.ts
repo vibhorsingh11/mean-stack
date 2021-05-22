82716d5cd45e70c7c52e5407aaac6d19cefee408
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-05-22 22:43:52 +0530

    Added post features

--- mean-stack/src/app/app.module.ts
+++ mean-stack/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, MaterialModule, BrowserAnimationsModule],
+  declarations: [AppComponent, PostCreateComponent, PostListComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ mean-stack/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent implements OnInit {
+  content = '';
+  title = '';
+  @Output() postCreated = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddPost() {
+    const post: Post = {
+      title: this.title,
+      content: this.content,
+    };
+    this.postCreated.emit(post);
+  }
+}
