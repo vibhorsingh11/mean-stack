2e32c5f6531dde0574f5b40b08225a4dcf231933
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-05-25 16:52:56 +0530

    updated getpost to get updated response

--- mean-stack/src/app/posts/posts.service.ts
+++ mean-stack/src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -13,11 +14,20 @@ export class PostService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
-        'http://localhost:3000/api/posts'
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          return postData.posts.map((post: any) => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id,
+            };
+          });
+        })
       )
-      .subscribe((postsData) => {
-        this.posts = postsData.posts;
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postUpdated.next([...this.posts]);
       });
   }
