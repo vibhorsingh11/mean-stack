ccaa00999bf9ad33f2eb63efe938b9639548e237
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-05-26 22:07:13 +0530

    added routing module

--- mean-stack/src/app/app.module.ts
+++ mean-stack/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { AppRoutingModule } from './app,routing.module';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent, PostListComponent],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     MaterialModule,
     BrowserAnimationsModule,
