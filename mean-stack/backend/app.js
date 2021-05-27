42c01ebfd1d81a9bd208ab1aa1310d99f1172d94
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-05-27 23:03:18 +0530

    seperated the post api routes

--- mean-stack/backend/app.js
+++ mean-stack/backend/app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// Reference from mongodb
-const Post = require('./models/post');
+const postRoutes = require("./routes/posts");
 
 const app = express();
 
@@ -29,57 +28,6 @@ app.use((req,res,next) => {
     next()
 });
 
-app.post("/api/posts", (req,res,next) => {
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
-    post.save().then(createdPost => {
-        res.status(201).json({
-            message: 'Post added successfully',
-            postId: createdPost._id
-        });
-    });
-});
-
-app.get("/api/posts",(req,res,next) => {
-    Post.find().then(documents => {
-        res.status(200).json({
-            message: "Posts fetched succesfully!",
-            posts: documents
-        });
-    });
-});
-
-app.get("/api/posts/:id",(req,res,next) => {
-    Post.findById(req.params.id).then(post => {
-        if(post){
-            res.status(200).json(post);
-        } else {
-            res.status(404).json({ message: 'Post not found.' });
-        }
-    });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content,
-    });
-    Post.updateOne({_id: req.body.id}, post).then(result => {
-        console.log(result);
-        res.status(200).json({ message: 'Post updated !!'})
-    })
-})
-
-app.delete("/api/posts/:id", (req,res,next) => {
-    Post.deleteOne({ _id: req.params.id }).then(result => {
-        console.log(result);
-        res.status(200).json({
-            message: 'Post added successfully'
-        });
-    });
-});
+app.use("/api/posts",postRoutes);
 
 module.exports = app;
