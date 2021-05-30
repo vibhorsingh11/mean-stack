f4c805e16d8e2f18877bf0c8865d9afe2d484ab1
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-05-30 17:06:12 +0530

    added multer to setup file upload

--- mean-stack/backend/routes/posts.js
+++ mean-stack/backend/routes/posts.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const multer = require("multer");
 
 const router = express.Router();
+
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg',
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = new Error('Invalid mime type');
+        if (isValid) {
+            error = null;
+        }
+        cb(null, "backend/images");
+    },
+    filename: (req, file, cb) => {
+        const name = file.originalname.toLowerCase().split(' ').join('-');
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, name + '-' + Date.now() + '-' + ext);
+    }
+});
 
 // Reference from mongodb
 const Post = require("../models/post");
 
-router.post("", (req,res,next) => {
+router.post("", multer(storage).single("image"), (req,res,next) => {
     const post = new Post({
         title: req.body.title,
         content: req.body.content
