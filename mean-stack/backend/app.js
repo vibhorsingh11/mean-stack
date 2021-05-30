180e7207604b0669e379ac434f43ccf878584edb
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-05-30 22:09:52 +0530

    update post-list to view image

--- mean-stack/backend/app.js
+++ mean-stack/backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -20,6 +21,7 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use(bodyParser.json())
+app.use("/images", express.static(path.join("backend/images")))
 
 app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin","*");
