cdf404b1ba375f34fbfa274848ea46bb83c6c836
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-06-13 15:36:27 +0530

    added token expiration

--- mean-stack/src/app/auth/auth.service.ts
+++ mean-stack/src/app/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { AuthData } from './auth-data.model';
 export class AuthService {
   private token: string;
   private isAuthenticated = false;
+  private tokenTimer: any;
   private authStateListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) {}
@@ -37,11 +38,19 @@ export class AuthService {
   login(email: string, password: string) {
     const authData: AuthData = { email, password };
     this.http
-      .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
+      .post<{ token: string; expiresIn: number }>(
+        'http://localhost:3000/api/user/login',
+        authData
+      )
       .subscribe((result) => {
         const token = result.token;
         this.token = token;
         if (token) {
+          const expiresInDuration = result.expiresIn;
+          console.log(expiresInDuration);
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration);
           this.isAuthenticated = true;
           this.authStateListener.next(true);
           this.router.navigate(['/']);
@@ -50,6 +59,7 @@ export class AuthService {
   }
 
   logout() {
+    clearTimeout(this.tokenTimer);
     this.token = null;
     this.isAuthenticated = false;
     this.authStateListener.next(false);
